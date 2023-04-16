3b93ad5a150214e7c7ecbae9bb64c12e153344ce
tyFeng <bofeng1997@gmail.com>
2023-04-16 17:49:48 +0800

    cap pass

--- kernel/object/cap_group.c
+++ kernel/object/cap_group.c
@@ -71,7 +71,8 @@ int cap_group_init(struct cap_group *cap_group, unsigned int size, u64 pid)
 {
         struct slot_table *slot_table = &cap_group->slot_table;
         /* LAB 3 TODO BEGIN */
-
+        cap_group->pid = pid;
+        return   slot_table_init(slot_table,size);
         /* LAB 3 TODO END */
         return 0;
 }
@@ -231,6 +232,7 @@ int sys_create_cap_group(u64 pid, u64 cap_group_name, u64 name_len, u64 pcid)
         }
         /* LAB 3 TODO BEGIN */
         /* cap current cap_group */
+        new_cap_group = obj_alloc(TYPE_CAP_GROUP,sizeof(*new_cap_group));
 
 
         /* LAB 3 TODO END */
@@ -240,6 +242,7 @@ int sys_create_cap_group(u64 pid, u64 cap_group_name, u64 name_len, u64 pcid)
                 goto out_fail;
         }
         /* LAB 3 TODO BEGIN */
+        cap_group_init(new_cap_group,BASE_OBJECT_NUM ,current_cap_group->pid);
 
         /* LAB 3 TODO END */
 
@@ -259,6 +262,7 @@ int sys_create_cap_group(u64 pid, u64 cap_group_name, u64 name_len, u64 pcid)
 
         /* 2st cap is vmspace */
         /* LAB 3 TODO BEGIN */
+        vmspace = obj_alloc(TYPE_VMSPACE,sizeof(*vmspace));
 
         /* LAB 3 TODO END */
         if (!vmspace) {
@@ -303,21 +307,26 @@ struct cap_group *create_root_cap_group(char *name, size_t name_len)
         struct vmspace *vmspace;
         int slot_id;
         /* LAB 3 TODO BEGIN */
+        cap_group = obj_alloc(TYPE_CAP_GROUP,sizeof(*cap_group));
 
         /* LAB 3 TODO END */
         BUG_ON(!cap_group);
         /* LAB 3 TODO BEGIN */
-
+        cap_group_init(cap_group,BASE_OBJECT_NUM ,ROOT_PID);
+        slot_id = alloc_slot_id(cap_group);
         /* LAB 3 TODO END */
         BUG_ON(slot_id != CAP_GROUP_OBJ_ID);
         /* LAB 3 TODO BEGIN */
+        vmspace = obj_alloc(TYPE_VMSPACE,sizeof(*vmspace));
 
         /* LAB 3 TODO END */
         BUG_ON(!vmspace);
 
         /* fixed PCID 1 for root process, PCID 0 is not used. */
         /* LAB 3 TODO BEGIN */
-
+        vmspace->pcid = ROOT_PCID;
+        vmspace_init(vmspace);
+        slot_id =  cap_alloc(cap_group,vmspace,0);
         /* LAB 3 TODO END */
         BUG_ON(slot_id != VMSPACE_OBJ_ID);
         /* Set the cap_group_name (process_name) for easing debugging */
