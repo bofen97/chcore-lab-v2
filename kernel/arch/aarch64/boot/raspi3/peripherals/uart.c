e305df261427252893e219de07b07b42d73bcbbe
tyFeng <bofeng1997@gmail.com>
2023-05-13 05:49:45 +0800

    added some comments to uart.c/early_uart_init

--- kernel/arch/aarch64/boot/raspi3/peripherals/uart.c
+++ kernel/arch/aarch64/boot/raspi3/peripherals/uart.c
@@ -37,23 +37,47 @@ void early_uart_init(void)
         unsigned int ra;
 
         ra = early_get32(GPFSEL1);
-        ra &= ~(7 << 12);
+        ra &= ~(7 << 12); 
         ra |= 2 << 12;
         ra &= ~(7 << 15);
         ra |= 2 << 15;
-        early_put32(GPFSEL1, ra);
+        /*
+        GPIO Function Select 1  
+        ra bin code :  00 010 010 000 000 000 000
+        FSEL 14 : GPIO Pin 14 takes alternate function 5
+        FSEL 15 : GPIO Pin 15 takes alternate function 5
 
-        early_put32(GPPUD, 0);
+        GIIO No. | Pull  | ALT0 | ALT1 | ALT2 | ALT3 | ALT4 | ALT5 |
+        GPIO 14  | Low   | TXD0 | SD6  | ---- | ---- | ---- | TXD1 |
+        GPIO 15  | Low   | RXD0 | SD7  | ---- | ---- | ---- | RXD1 |
+
+        -- TXD1 UART 1 Transmit Data
+        -- RXD1 UART 1 Receive Data 
+        */         
+        early_put32(GPFSEL1, ra); 
+        
+
+        //Write to GPPUD to set the required control signal (i.e. Pull-up or Pull-Down or neither 
+        //to remove the current Pull-up/down)
+        early_put32(GPPUD, 0); 
+        //Wait 150 cycles – this provides the required set-up time for the control signal 
         delay(150);
-        early_put32(GPPUDCLK0, (1 << 14) | (1 << 15));
+        //Write  to  GPPUDCLK0/1  to  clock  the  control  signal into  the  GPIO  pads  you  wish  to 
+        //modify 
+        early_put32(GPPUDCLK0, (1 << 14) | (1 << 15)); //Assert Clock on line , pin 14 and 15
+        //Wait 150 cycles 
         delay(150);
+        //Write to GPPUD to remove the control signal
+        early_put32(GPPUD, 0); 
+        // Write to GPPUDCLK0/1 to remove the clock
         early_put32(GPPUDCLK0, 0);
+
 
         early_put32(AUX_ENABLES, 1);
         early_put32(AUX_MU_IER_REG, 0);
         early_put32(AUX_MU_CNTL_REG, 0);
         early_put32(AUX_MU_IER_REG, 0);
-        early_put32(AUX_MU_LCR_REG, 3);
+        early_put32(AUX_MU_LCR_REG, 3); // set bit  11 : the UART works in 8-bit mode
         early_put32(AUX_MU_MCR_REG, 0);
         early_put32(AUX_MU_BAUD_REG, 270);
 
@@ -68,10 +92,14 @@ static unsigned int early_uart_lsr(void)
 static void early_uart_send(unsigned int c)
 {
         while (1) {
-                if (early_uart_lsr() & 0x20)
+                if (early_uart_lsr() & 0x20) /* This bit is set if the transmit FIFO is empty and the 
+transmitter is idle. (Finished shifting out the last bit) */
                         break;
         }
-        early_put32(AUX_MU_IO_REG, c);
+        early_put32(AUX_MU_IO_REG, c); /*If the DLAB bit in the line control register is set this register gives access to the LS 8 bits 
+of the baud rate.
+
+        */
 }
 
 void uart_send_string(char *str)
